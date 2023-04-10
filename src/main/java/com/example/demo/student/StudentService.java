79cf8dace30f3e8dde2a92c24ddb69488922a54d
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-04-10 14:25:35 +0200

    add student service

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,21 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(1990, Month.APRIL, 23),
+                        33
+                )
+        );
+    }
 }
+
