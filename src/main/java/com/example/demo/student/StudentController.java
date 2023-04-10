c50db670a31ad693e98c1f4ee789113c6d5cf788
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-04-10 17:16:35 +0200

    add bean for student service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,10 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+    // Autowired is used for dependency injection
+    @Autowired
     public StudentController(StudentService studentService) {
-        this.studentService = studentService;
+        this.studentService =  studentService;
     }
 
     @GetMapping
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,14 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+//Component declared this class is bean
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
